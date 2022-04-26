a9918929addb931b759b05cfa02b17f8628af27d
Chaedie <88099590+Chaedie@users.noreply.github.com>
2022-04-27 08:31:37 +0900

    Add sum.java

--- src/chaedie/calender/Calender.java
+++ src/chaedie/calender/Calender.java
@@ -3,9 +3,6 @@ package chaedie.calender;
 public class Calender {
 
 	public static void main(String[] args) {
-		System.out.println("");
-		System.out.println("s\rs\ns");
-
 		System.out.println(" 일  월  화  수  목  금  토");
 		System.out.println("---------------------------");
 		System.out.println("  1   2   3   4   5   6   7");
@@ -13,5 +10,4 @@ public class Calender {
 		System.out.println("15 16 17 18 19 20 21");
 		System.out.println("22 23 24 25 26 27 28");
 	}
-
 }
