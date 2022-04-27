07711a61c16a270b11afe31a35bd2baaf12524f7
Chaedie <88099590+Chaedie@users.noreply.github.com>
2022-04-27 10:54:08 +0900

    Feat : add function (print calendar of input month)

--- src/chaedie/calendar/Calendar.java
+++ src/chaedie/calendar/Calendar.java
@@ -12,18 +12,28 @@ public class Calendar {
 
 	}
 
-	public void printSampleCalendar() {
+	public void printSampleCalendar(int month) {
+		Calendar cal = new Calendar();
+
+		System.out.println();
 		System.out.println(" 일  월  화  수  목  금  토");
 		System.out.println("---------------------------");
 		System.out.println("  1   2   3   4   5   6   7");
 		System.out.println("  8   9 10 11 12 13 14");
 		System.out.println("15 16 17 18 19 20 21");
 		System.out.println("22 23 24 25 26 27 28");
+		if (cal.getMaxDaysOfMonth(month) == 30) {
+			System.out.println("29 30");
+
+		} else if (cal.getMaxDaysOfMonth(month) == 31) {
+			System.out.println("29 30 31");
+		}
+		System.out.println();
 	}
 
 	public static void main(String[] args) {
 
-		// 프롬프트
+		// 프롬프트 추가
 		String PROMPT = "cal> ";
 		Scanner scan = new Scanner(System.in);
 		Calendar cal = new Calendar();
@@ -33,19 +43,19 @@ public class Calendar {
 			System.out.print(PROMPT);
 			int iMonth = scan.nextInt();
 
+			// 예외처리
 			if (iMonth == -1) {
 				System.out.print("Bye ~");
 				break;
 			}
-			
-			if (iMonth > 12 || iMonth < -1 || iMonth == 0 ) {
-				System.out.println("Wrong value");
+
+			if (iMonth > 12 || iMonth < -1 || iMonth == 0) {
+				System.out.println("Wrong value \n");
 				continue;
 			}
-			System.out.printf("%d월의 마지막 날은 %d 입니다. \n", iMonth, cal.getMaxDaysOfMonth(iMonth));
-		}
 
-//		cal.printSampleCalendar();
+			cal.printSampleCalendar(iMonth);
+		}
 		scan.close();
 
 	}
